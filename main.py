3acdc29d05689e2821b1613dd5149c698f338824
Daniu13 <daniel.lozano1@udea.edu.co>
2024-03-07 17:43:20 -0500

    clase marcapasos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 "Hallo"
+import datetime
 
 class Implante:
     def __init__(self) -> None:
@@ -9,25 +10,25 @@ class Implante:
         self.__fecha_mantenimiento = None
 
     def ver_fecha_implantacion(self):
-        pass
+        return self.__fecha_implantacion
     def ver_medico(self):
-        pass
+        return self.__medico
     def ver_estado(self):
-        pass
+        return self.__estado
     def ver_fecha_revision(self):
-        pass
+        return self.__fecha_revision
     def ver_fecha_mantenimiento(self):
-        pass
-    def set_fecha_implantacion(self):
-        pass
-    def set_medico(self):
-        pass
-    def set_estado(self):
-        pass
-    def set_fecha_revision(self):
-        pass
-    def set_fecha_mantenimiento(self):
-        pass
+        return self.__fecha_mantenimiento
+    def set_fecha_implantacion(self, fecha):
+        self.__fecha_implantacion = fecha
+    def set_medico(self, medico):
+        self.__medico = medico
+    def set_estado(self, estado):
+        self.__estado = estado
+    def set_fecha_revision(self, fecha):
+        self.__fecha_revision = fecha
+    def set_fecha_mantenimiento(self, fecha):
+        self.__fecha_mantenimiento = fecha
 
 class Paciente:
     def __init__(self) -> None:
@@ -35,10 +36,29 @@ class Paciente:
         self.__implantes = set()
 
     def ver_id(self):
-        pass
-    def set_id(self):
-        pass
-    def agregar_implante(self):
-        pass
-    def eliminar_implante(self):
-        pass
+        return self.__id
+    def set_id(self, id):
+        self.__id = id
+    def agregar_implante(self, implante):
+        self.__implantes.add(implante)
+    def eliminar_implante(self, implante):
+        self.__implantes.discard(implante)
+
+class Marcapasos:
+    def __init__(self) -> None:
+        self.__num_electrodos = None
+        self.__es_inalambrico = None
+        self.__frecuencia_estimulacion = None
+    
+    def ver_num_eletrodos(self):
+        return self.__num_electrodos
+    def ver_es_alambrico(self):
+        return self.__es_inalambrico
+    def ver_frecuencia_estimulacion(self):
+        return self.__frecuencia_estimulacion
+    def set_num_eletrodos(self, num):
+        self.__num_electrodos = num
+    def set_es_alambrico(self, es):
+        self.__es_inalambrico = es
+    def set_frecuencia_estimulacion(self, frecuencia):
+        self.__frecuencia_estimulacion = frecuencia
